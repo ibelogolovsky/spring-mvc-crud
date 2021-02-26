09d6d0946e3983611271e352a4a225f30df2e1ec
Igor Belogolovsky <igor@belogolovsky.net>
2021-02-26 23:32:37 +0300

    Save method refactored

--- src/main/java/crud/dao/UserRepository.java
+++ src/main/java/crud/dao/UserRepository.java
@@ -6,8 +6,6 @@ import java.util.List;
 //import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository {
-    User get(String model, int series);
-
     User findById(long id);
 
     void save(User user);
--- src/main/java/crud/dao/UserRepositoryImpl.java
+++ src/main/java/crud/dao/UserRepositoryImpl.java
@@ -18,22 +18,18 @@ public class UserRepositoryImpl implements UserRepository{
     }
 
     @Override
-    public User get(String model, int series) {
-        return null;
-    }
-
-    @Override
     public User findById(long id) {
         EntityManager em = emf.createEntityManager();
-        User result = em.find(User.class, id);
-        return result;
+        User user = em.find(User.class, id);
+        em.detach(user);
+        return user;
     }
 
     @Override
     public void save(User user) {
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
-        em.persist(user);
+        em.merge(user);
         em.getTransaction().commit();
         em.close();
 
@@ -52,8 +48,6 @@ public class UserRepositoryImpl implements UserRepository{
     @Override
     public List<User> findAll() {
         EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        em.createQuery("SELECT from " + User.class.getSimpleName()).getResultList();
-        return null;
+        return em.createQuery("SELECT u from User u").getResultList();
     }
 }
